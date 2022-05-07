747bedab2523680d219b64dbd795ba8bb7a8d97c
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2022-05-07 19:18:32 +0530

    a=int(input()) b=int(input()) if a<9:  print(2)  print(3)  print(5)  print(7)  a=10 for i in range(a,b+1):
      for j in range(2,a):
       if i%j==0:
        break
      else:
        print(i)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=int(input())
+if a<9:
+ print(2)
+ print(3)
+ print(5)
+ print(7)
+ a=10
+for i in range(a,b+1):
+  
+  for j in range(2,a):
+   if i%j==0:
+    break
+  else:
+    print(i)
