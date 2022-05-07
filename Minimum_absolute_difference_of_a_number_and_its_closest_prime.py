db456d447bb9dbadb47d58cbcbeb644683d72169
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2022-05-07 18:40:46 +0530

    k=int(input()) for i in range(k,2,-1):     for j in range(2,i):      if i%j==0:       #print(i)       break     else:       break for m in range(k+1,k+100):     for n in range(2,m):      if m%n==0:       #print(i)       break     else:       break if abs(k-m)>=abs(k-i):      print(abs(k-i)) else:      print(abs(k-m))

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,20 @@
+
+k=int(input())
+for i in range(k,2,-1):
+    for j in range(2,i):
+     if i%j==0:
+      #print(i)
+      break
+    else:
+      break
+for m in range(k+1,k+100):
+    for n in range(2,m):
+     if m%n==0:
+      #print(i)
+      break
+    else:
+      break 
+if abs(k-m)>=abs(k-i):
+     print(abs(k-i))
+else:
+     print(abs(k-m))
