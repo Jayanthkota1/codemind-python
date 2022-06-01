c9475e428cfebe192d16df5de29d06a530456df4
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2022-06-01 10:43:42 +0530

    n=int(input()) a=[] while n:     d=n%10     a.append(d)     n//=10 a=a[::-1] for i in range(len(a)):     if a[i]==6:         a[i]=9         break r=0 for i in range(len(a)):    r=r*10+a[i] print(r)

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,17 +1,16 @@
 n=int(input())
-k=n
 a=[]
-while k:
-    d=k%10
+while n:
+    d=n%10
     a.append(d)
-    k//=10
-a=a[::-1]    
+    n//=10
+a=a[::-1]
 for i in range(len(a)):
     if a[i]==6:
         a[i]=9
         break
 r=0    
 for i in range(len(a)):
-    r=r*10+a[i]
-print(r)    
+   r=r*10+a[i]
+print(r)   
     
