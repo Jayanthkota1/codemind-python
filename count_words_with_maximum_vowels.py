ab8b1247ac5b6f2949e8c1b3a1d0f5b56077ace2
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2022-06-21 09:44:04 +0530

    x=input() x=x+' ' c=0 k=0 min=0 a='aeiouAEIOU' for i in x:     if i==' ':         if min<c:             min=c         c=0     if i in a:         c+=1 for i in x:     if i==' ':         if min==c:             k+=1         c=0     if i in a:         c+=1 print(k)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,22 @@
+x=input()
+x=x+' '
+c=0
+k=0
+min=0
+a='aeiouAEIOU'
+for i in x:
+    if i==' ':
+        if min<c:
+            min=c
+        c=0
+    if i in a:
+        c+=1
+for i in x:
+    if i==' ':
+        if min==c:
+            k+=1
+        c=0
+    if i in a:
+        c+=1   
+print(k)    
+    
