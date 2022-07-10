4f5594834575994ca27375c4a82d32663b7caaff
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2022-07-10 19:29:21 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) c=0 a=set(a) b=set(b) for i in a:     if i in b:         c+=1 print(c)

---
+++ Count_elements_I.py
@@ -0,0 +1,10 @@
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=0
+a=set(a)
+b=set(b)
+for i in a:
+    if i in b:
+        c+=1
+print(c)        
