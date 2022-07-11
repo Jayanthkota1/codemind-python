0a701f2ca2bc9afbe81822cf677567ef5a968ee5
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2022-07-11 10:23:06 +0530

    s=input() s1=input() s=s.lower() s1=s1.lower() s=s.split() s1=s1.split() c=0 for i in s:     if i in s1:         c+=1 print(c)

--- common_words_-1.py
+++ common_words_-1.py
@@ -1,11 +1,11 @@
-x=input()
-y=input()
-x=x.lower()
-y=y.lower()
-x=x.split()
-y=y.split()
+s=input()
+s1=input()
+s=s.lower()
+s1=s1.lower()
+s=s.split()
+s1=s1.split()
 c=0
-for i in x:
-    if i in y:
+for i in s:
+    if i in s1:
         c+=1
-print(c)        
+print(c)
