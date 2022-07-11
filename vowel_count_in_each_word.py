3cf1f0fea7e02bdb570ccdd3c404d515ae49dab6
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2022-07-11 09:59:03 +0530

    s=input() s=s.lower() s=s.split() a='aeiou' c=0 max=0 for i in s:     c=0     for j in i:         if j in a:             c+=1     print(c,end=' ')

--- vowel_count_in_each_word.py
+++ vowel_count_in_each_word.py
@@ -1,12 +1,12 @@
-x=input()
-x=x+' '
+s=input()
+s=s.lower()
+s=s.split()
+a='aeiou'
 c=0
-a='aeiouAEIOU'
-for i in x:
-    if i==' ':
-        print(c,end=' ')
-        c=0
-    if i in a:
-        c+=1
-    
-    
+max=0
+for i in s:
+    c=0
+    for j in i:
+        if j in a:
+            c+=1
+    print(c,end=' ')        
